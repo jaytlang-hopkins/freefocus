118b4b9fdf9a9b7e4314ed95e5b246887083743a
Jay Lang <jaytlang9@gmail.com>
2025-08-06 10:09:36 -0400

    :page_facing_up: docs: make license text consistent

--- cli.py
+++ cli.py
@@ -1,3 +1,18 @@
+# File: cli.py
+# Copyright 2025 Jay Lang
+# 
+# This file is part of the EyeMotion project. EyeMotion is free software: you
+# can redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# EyeMotion is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with
+# EyeMotion. If not, see <https://www.gnu.org/licenses/>. 
+
 from dataclasses import dataclass
 from typing import Callable, List, Union
 
