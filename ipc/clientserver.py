656e65fb58ee359fd7d9f8a963617661d02803ae
Jay <jlang20@jh.edu>
2025-10-03 23:20:00 -0400

    :page_facing_up: chore: add license text to new files

--- ipc/clientserver.py
+++ ipc/clientserver.py
@@ -1,4 +1,17 @@
-# Rough draft!
+# File: ipc/clientserver.py
+# Copyright 2025 Jay Lang
+# 
+# This file is part of the FreeFocus project. FreeFocus is free software: you
+# can redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# FreeFocus is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with
+# FreeFocus. If not, see <https://www.gnu.org/licenses/>. 
 from dataclasses import dataclass
 from typing import Callable, List, Union
 
